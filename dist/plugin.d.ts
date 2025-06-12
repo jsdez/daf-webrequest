9e3e39f5a7958bb814c7d8caf2e9496276093055
Jake Dennison <jake@neoautomate.com>
2025-06-12 14:22:50 +0100

    Update distributable files

---
+++ dist/plugin.d.ts
@@ -0,0 +1,11 @@
+import { LitElement } from 'lit';
+import { PluginContract } from '@nintex/form-plugin-contract';
+export declare class DafWebRequestPlugin extends LitElement {
+    label: string;
+    description: string;
+    readOnly: boolean;
+    value: string;
+    static getMetaConfig(): PluginContract;
+    render(): import("lit-html").TemplateResult<1>;
+    private onInput;
+}
