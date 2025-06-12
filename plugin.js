f280f1a5cbe0bb311044053103f7dcbaf89d9b28
Jake Dennison <jake@neoautomate.com>
2025-06-12 14:20:48 +0100

    Add distributable plugin.js to root for GitHub Pages

---
+++ plugin.js
@@ -0,0 +1,77 @@
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+import { html, LitElement } from 'lit';
+import { customElement, property } from 'lit/decorators.js';
+let DafWebRequestPlugin = class DafWebRequestPlugin extends LitElement {
+    constructor() {
+        super(...arguments);
+        this.label = '';
+        this.description = '';
+        this.readOnly = false;
+        this.value = '';
+        this.onInput = (e) => {
+            const input = e.target;
+            this.value = input.value;
+            this.dispatchEvent(new CustomEvent('ntx-value-change', {
+                detail: this.value,
+                bubbles: true,
+                composed: true,
+            }));
+        };
+    }
+    static getMetaConfig() {
+        return {
+            controlName: 'Web Request Plugin',
+            fallbackDisableSubmit: false,
+            version: '1.0.0',
+            description: 'A Nintex Form Plugin for making API calls.',
+            properties: {
+                value: {
+                    type: 'string',
+                    title: 'Value',
+                    isValueField: true,
+                    defaultValue: '',
+                },
+            },
+            standardProperties: {
+                fieldLabel: true,
+                description: true,
+                readOnly: true,
+                defaultValue: true,
+            },
+        };
+    }
+    render() {
+        return html `
+      <div>
+        <label>${this.label}</label>
+        <input
+          .value=${this.value}
+          ?disabled=${this.readOnly}
+          @input=${this.onInput}
+        />
+        <div>${this.description}</div>
+      </div>
+    `;
+    }
+};
+__decorate([
+    property({ type: String })
+], DafWebRequestPlugin.prototype, "label", void 0);
+__decorate([
+    property({ type: String })
+], DafWebRequestPlugin.prototype, "description", void 0);
+__decorate([
+    property({ type: Boolean })
+], DafWebRequestPlugin.prototype, "readOnly", void 0);
+__decorate([
+    property({ type: String })
+], DafWebRequestPlugin.prototype, "value", void 0);
+DafWebRequestPlugin = __decorate([
+    customElement('daf-webrequest-plugin')
+], DafWebRequestPlugin);
+export { DafWebRequestPlugin };
